2d8987083ac6048fa271f6be10e6c7295a5c9761
Rob Shapiro <robshaps78@gmail.com>
2013-03-04 21:13:41 -0500

    Created a rails app with testimonials

---
+++ app/models/testimonial.rb
@@ -0,0 +1,3 @@
+class Testimonial < ActiveRecord::Base
+  attr_accessible :comments, :name, :rating
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fongu::Application.config.secret_token = '08b74bda760482bb612dc9097f494cd0b29d60da00b07503bdc4581a591cd36d208278fbadb26ae380c44be46e9fa3c3a2ad5d5682c825b33535340a753af4e9'
